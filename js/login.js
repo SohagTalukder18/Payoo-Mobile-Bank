29e3fb233e7429cb732a0f0b33458730c294e0ba
SohagTalukder18 <sohag.cse180818@gmail.com>
2024-09-22 14:28:05 +0600

    added all file and login section

---
+++ js/login.js
@@ -0,0 +1,11 @@
+
+
+
+document.getElementById("btn-login").addEventListener('click',function(e){
+    e.preventDefault();
+    
+    const phoneNumber = document.getElementById("phone-number").value;
+    console.log(phoneNumber);
+    
+    
+})
